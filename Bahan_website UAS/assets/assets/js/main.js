29cc3a0df2016c33ade1a026a277024b35716465
sitiolis <sitiolis06@gmail.com>
2022-01-28 09:38:26 +0700

    mtsyaspim

---
+++ Bahan_website UAS/assets/assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+            $("#slider-hero").owlcarousel({
+                loop: true;
+                nav: true;
+                // mouseDrag: false;
+                items: 1,
+                dots: false,
+                margin: 0,
+                navText: [
+                    "<i class="
+                    fa fa - angel - left aria - hidden = true "> <i>",
+                    "<i class="
+                    fa fa - angel - right aria - hidden = true "> <i>",
+                ],
+                navContainer: '#hero-area-nav ',
+            });
+            $("#tenaga-pendidik-slider").owlcarousel({
+                loop: true;
+                nav: true;
+                // mouseDrag: false;
+                items: 1,
+                dots: false,
+                margin: 30,
+                navText: [
+                    "<i class="
+                    fa fa - angel - left aria - hidden = true "> <i>",
+                    "<i class="
+                    fa fa - angel - right aria - hidden = true "> <i>",
+                ],
+                navcontainer: '#hero-area-nav ',
+            });
